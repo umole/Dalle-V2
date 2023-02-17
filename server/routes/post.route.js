ea43a25a14893b7bceefa14e4df8b6aafa4e4dcc
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-02-17 09:43:26 +0100

    CREATING CLOUDINARY

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -10,4 +10,11 @@ import PostSchema from '../mongodb/models/post.js';
 dotenv.config();
 
 const router = express.Router();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 export default router;
